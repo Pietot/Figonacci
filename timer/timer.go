2a220d29afcd9724af5f13e5ea902f955f149b4e
Pietot <102545552+Pietot@users.noreply.github.com>
2024-12-14 21:53:39 +0100

    Added big.int and multiple returns to TimeNumber function

--- timer/timer.go
+++ timer/timer.go
@@ -25,11 +25,13 @@ func Timer(f func(int) *big.Int) (int, string, string, string) {
 	return number, fibonacciNumberString, fibonacciNumberStringLen, computeTimeElapsed
 }
 
-func TimeNumber(f func(int) int, number int) int {
-	fibonacciNumber := 0
+func TimeNumber(f func(int) *big.Int, number int) (string, string, string) {
+	fibonacciNumber := new(big.Int)
 	start := time.Now()
 	fibonacciNumber = f(number)
 	elapsed := time.Since(start)
-	fmt.Println(elapsed)
-	return fibonacciNumber
+	fibonacciNumberString := fibonacciNumber.String()
+	fibonacciNumberStringLen := fmt.Sprintf("%d digits", len(fibonacciNumberString))
+	computeTimeElapsed := fmt.Sprintf("%.2fs", elapsed.Seconds())
+	return fibonacciNumberString, fibonacciNumberStringLen, computeTimeElapsed
 }
