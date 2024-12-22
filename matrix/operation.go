61be1bf099430cff4d5ce11ee14f92f301fa35bd
Pietot <102545552+Pietot@users.noreply.github.com>
2024-12-22 15:34:30 +0100

    Added operation.go to include matrix package

---
+++ matrix/operation.go
@@ -0,0 +1,46 @@
+package matrix
+
+import "math/big"
+
+type Matrix2x2 [2][2]*big.Int
+
+func (m *Matrix2x2) Multiply(a, b *Matrix2x2) *Matrix2x2 {
+	return &Matrix2x2{
+		[2]*big.Int{
+			new(big.Int).Add(new(big.Int).Mul(a[0][0], b[0][0]), new(big.Int).Mul(a[0][1], b[1][0])),
+			new(big.Int).Add(new(big.Int).Mul(a[0][0], b[0][1]), new(big.Int).Mul(a[0][1], b[1][1])),
+		},
+		[2]*big.Int{
+			new(big.Int).Add(new(big.Int).Mul(a[1][0], b[0][0]), new(big.Int).Mul(a[1][1], b[1][0])),
+			new(big.Int).Add(new(big.Int).Mul(a[1][0], b[0][1]), new(big.Int).Mul(a[1][1], b[1][1])),
+		},
+	}
+}
+
+func (m *Matrix2x2) Pow(n int) *Matrix2x2 {
+	result := &Matrix2x2{
+		[2]*big.Int{big.NewInt(1), big.NewInt(0)},
+		[2]*big.Int{big.NewInt(0), big.NewInt(1)},
+	}
+	base := m
+	for i := 0; i < n; i++ {
+		result = result.Multiply(result, base)
+	}
+	return result
+}
+
+func (m *Matrix2x2) FastPow(n int) *Matrix2x2 {
+	result := &Matrix2x2{
+		[2]*big.Int{big.NewInt(1), big.NewInt(0)},
+		[2]*big.Int{big.NewInt(0), big.NewInt(1)},
+	}
+	base := m
+	for n > 0 {
+		if n%2 == 1 {
+			result = result.Multiply(result, base)
+		}
+		n >>= 1
+		base = base.Multiply(base, base)
+	}
+	return result
+}
