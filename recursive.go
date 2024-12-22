7c26c45a9d7f41f5822b7f17c477c1de834e94fd
Pietot <102545552+Pietot@users.noreply.github.com>
2024-12-22 12:50:06 +0100

    Added goroutine

--- recursive.go
+++ recursive.go
@@ -6,15 +6,22 @@ import (
 )
 
 func FibonacciRecursive(n int, ctx context.Context) *big.Int {
+	result := big.NewInt(0)
+	done := make(chan struct{})
+
+	go func() {
+		defer close(done)
+		if n <= 1 {
+			result.SetInt64(int64(n))
+			return
+		}
+		result.Add(FibonacciRecursive(n-1, ctx), FibonacciRecursive(n-2, ctx))
+	}()
+
 	select {
 	case <-ctx.Done():
 		return big.NewInt(0)
-	default:
-		if n <= 1 {
-			return big.NewInt(int64(n))
-		}
-		a := FibonacciRecursive(n-1, ctx)
-		b := FibonacciRecursive(n-2, ctx)
-		return new(big.Int).Add(a, b)
+	case <-done:
+		return result
 	}
 }
