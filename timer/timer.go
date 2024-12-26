9e0d8c9ac71a4a2835eeb748c50849e0d1a8d389
Baptiste <baptiste.blasquez@gmail.com>
2024-12-26 17:14:12 +0100

    Added better message to return

--- timer/timer.go
+++ timer/timer.go
@@ -7,7 +7,8 @@ import (
 	"time"
 )
 
-func Timer(f func(int, context.Context) *big.Int) (int, string, string, string) {
+func Timer(f func(int, context.Context) *big.Int) (string) {
+	fibonacciNumber := new(big.Int)
 	lowNumber, highNumber := 0, 1
 	computeTimeStart := time.Now()
 
@@ -26,7 +27,6 @@ func Timer(f func(int, context.Context) *big.Int) (int, string, string, string) 
 	}
 
 	// Binary search to find the biggest number that takes less than 1 second to compute
-	var fibonacciNumber *big.Int
 	for lowNumber <= highNumber {
 		mid := (lowNumber + highNumber) / 2
 		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
@@ -42,19 +42,35 @@ func Timer(f func(int, context.Context) *big.Int) (int, string, string, string) 
 	}
 
 	fibonacciNumberString := fibonacciNumber.String()
-	fibonacciNumberStringLen := fmt.Sprintf("%d digits", len(fibonacciNumberString))
-	computeTimeElapsed := fmt.Sprintf("%.2fs", time.Since(computeTimeStart).Seconds())
-	return highNumber, fibonacciNumberString, fibonacciNumberStringLen, computeTimeElapsed
+	computeTimeElapsed := time.Since(computeTimeStart).Seconds()
+
+	sentence := fmt.Sprintf(
+		"\nThe biggest Fibonacci number that has been computed in less than 1 second is the \033[32m%dnth\033[0m Fibonacci number\n\n"+
+			"It's value is :\n\033[32m%s\033[0m\n\n"+
+			"It has \033[32m%d\033[0m digits.\n\n"+
+			"It has been found in \033[32m%.3f\033[0m seconds",
+		highNumber, fibonacciNumberString, len(fibonacciNumberString), computeTimeElapsed,
+	)
+
+	return sentence
 }
 
-func TimeNumber(f func(int, context.Context) *big.Int, number int) (string, string, string) {
+func TimeNumber(f func(int, context.Context) *big.Int, number int) string {
 	ctx := context.Background()
 	fibonacciNumber := new(big.Int)
 	start := time.Now()
 	fibonacciNumber = f(number, ctx)
 	elapsed := time.Since(start)
+
 	fibonacciNumberString := fibonacciNumber.String()
-	fibonacciNumberStringLen := fmt.Sprintf("%d digits", len(fibonacciNumberString))
 	computeTimeElapsed := fmt.Sprintf("%.2fs", elapsed.Seconds())
-	return fibonacciNumberString, fibonacciNumberStringLen, computeTimeElapsed
+	
+	sentence := fmt.Sprintf(
+		"\nThe \033[33m%d\033[0mnth Fibonacci number is :\n\033[33m%s\033[0m\n\n"+
+			"It has \033[33m%d\033[0m digits.\n\n"+
+			"It has been found in \033[33m%s\033[0m",
+		number, fibonacciNumberString, len(fibonacciNumberString), computeTimeElapsed,
+	)
+	
+	return sentence
 }
