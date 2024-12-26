de12add6c99449430bd4223616756cdc78d73757
Baptiste <baptiste.blasquez@gmail.com>
2024-12-26 17:25:07 +0100

    Removed start and elapsed variable

--- timer/timer.go
+++ timer/timer.go
@@ -7,21 +7,20 @@ import (
 	"time"
 )
 
-func Timer(f func(int, context.Context) *big.Int) (string) {
+func Timer(f func(int, context.Context) *big.Int) string {
 	fibonacciNumber := new(big.Int)
+	zero := big.NewInt(0)
 	lowNumber, highNumber := 0, 1
 	computeTimeStart := time.Now()
 
 	// Initial test to find the range of numbers that takes less than 1 second to compute
 	for {
 		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-		start := time.Now()
-		f(highNumber, ctx)
-		elapsed := time.Since(start)
-		cancel()
-		if elapsed > time.Second {
+		if f(highNumber, ctx).Cmp(zero) == 0 {
+			cancel()
 			break
 		}
+		cancel()
 		lowNumber = highNumber
 		highNumber *= 2
 	}
@@ -30,14 +29,13 @@ func Timer(f func(int, context.Context) *big.Int) (string) {
 	for lowNumber <= highNumber {
 		mid := (lowNumber + highNumber) / 2
 		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-		start := time.Now()
 		fibonacciNumber = f(mid, ctx)
-		elapsed := time.Since(start)
-		cancel()
-		if elapsed < time.Second {
-			lowNumber = mid + 1
-		} else {
+		if fibonacciNumber.Cmp(zero) == 0 {
+			cancel()
 			highNumber = mid - 1
+		} else {
+			cancel()
+			lowNumber = mid + 1
 		}
 	}
 
@@ -64,13 +62,13 @@ func TimeNumber(f func(int, context.Context) *big.Int, number int) string {
 
 	fibonacciNumberString := fibonacciNumber.String()
 	computeTimeElapsed := fmt.Sprintf("%.2fs", elapsed.Seconds())
-	
+
 	sentence := fmt.Sprintf(
 		"\nThe \033[33m%d\033[0mnth Fibonacci number is :\n\033[33m%s\033[0m\n\n"+
 			"It has \033[33m%d\033[0m digits.\n\n"+
 			"It has been found in \033[33m%s\033[0m",
 		number, fibonacciNumberString, len(fibonacciNumberString), computeTimeElapsed,
 	)
-	
+
 	return sentence
 }
