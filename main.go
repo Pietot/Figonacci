cd25ad0988ed806296f6171535c3d9b69c0a2fe9
Pietot <102545552+Pietot@users.noreply.github.com>
2024-12-27 23:38:26 +0100

    Added alias

--- main.go
+++ main.go
@@ -50,12 +50,12 @@ func printUsage() {
 	fmt.Println("")
 	fmt.Println("    Available algorithms (from \033[31mslowest\033[0m to \033[34mfastest\033[0m):")
 	fmt.Println("")
-	fmt.Println("        1 - \033[31mrecursive\033[0m")
-	fmt.Println("        2 - \033[33mrecursive_optimized\033[0m")
-	fmt.Println("        3 - \033[33miterative\033[0m")
-	fmt.Println("        4 - \033[32mmatrix\033[0m")
-	fmt.Println("        5 - \033[36mmatrix_optimized\033[0m")
-	fmt.Println("        6 - \033[34mfield_extension\033[0m")
+	fmt.Println("        1 - \033[31mrecursive (r)\033[0m")
+	fmt.Println("        2 - \033[33mrecursive_optimized (ro)\033[0m")
+	fmt.Println("        3 - \033[33miterative (i)\033[0m")
+	fmt.Println("        4 - \033[32mmatrix (m)\033[0m")
+	fmt.Println("        5 - \033[36mmatrix_optimized (mo)\033[0m")
+	fmt.Println("        6 - \033[34mfield_extension (fe)\033[0m")
 	fmt.Println("")
 }
 
@@ -115,11 +115,17 @@ func handleCompute(args []string) error {
 func isValidAlgorithm(algorithm string) func(int, context.Context) *big.Int {
 	validAlgorithms := map[string]func(int, context.Context) *big.Int{
 		"recursive":           FibonacciRecursive,
+		"r":                   FibonacciRecursive,
 		"recursive_optimized": FibonacciRecursiveOptimized,
+		"ro":                  FibonacciRecursiveOptimized,
 		"iterative":           FibonacciIterative,
+		"i":                   FibonacciIterative,
 		"matrix":              FibonacciMatrix,
+		"m":                   FibonacciMatrix,
 		"matrix_optimized":    FibonacciMatrixOptimized,
+		"mo":                  FibonacciMatrixOptimized,
 		"field_extension":     FibonacciFieldExtension,
+		"fe":                  FibonacciFieldExtension,
 	}
 
 	if function, exists := validAlgorithms[algorithm]; exists {
