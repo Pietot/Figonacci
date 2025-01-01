fbee69f52ccd5bfc3f49b60f577a1e58d937b598
Pietot <102545552+Pietot@users.noreply.github.com>
2025-01-01 20:35:42 +0100

    Minor changes

--- timer/timer_test.go
+++ timer/timer_test.go
@@ -14,11 +14,11 @@ func TestTimer(test *testing.T) {
 		limit    float64
 	}{
 		{"Recursive", algorithms.FibonacciRecursive, 1},
-		{"Recursive optimized", algorithms.FibonacciRecursiveOptimized, 1},
+		{"RecursiveOptimized", algorithms.FibonacciRecursiveOptimized, 1},
 		{"Iterative", algorithms.FibonacciIterative, 1},
 		{"Matrix", algorithms.FibonacciMatrix, 1},
-		{"Matrix optimized", algorithms.FibonacciMatrixOptimized, 1},
-		{"Field extension", algorithms.FibonacciFieldExtension, 1},
+		{"MatrixOptimized", algorithms.FibonacciMatrixOptimized, 1},
+		{"FieldExtension", algorithms.FibonacciFieldExtension, 1},
 	}
 
 	for _, unitTest := range algos {
