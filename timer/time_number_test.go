973591ed7de96612ebe37d7b234789d41ee1aa0d
Pietot <102545552+Pietot@users.noreply.github.com>
2025-01-01 18:25:47 +0100

    Added TestMatrixOptimizedResult function

--- timer/time_number_test.go
+++ timer/time_number_test.go
@@ -190,3 +190,31 @@ func TestIterativeResult(test *testing.T) {
 		})
 	}
 }
+
+func TestMatrixOptimizedResult(test *testing.T) {
+	unitTests := []struct {
+		name     string
+		function func(int, context.Context) *big.Int
+		number   int
+	}{
+		{"MatrixOptimized", algorithms.FibonacciMatrixOptimized, 0},
+		{"MatrixOptimized", algorithms.FibonacciMatrixOptimized, 1},
+		{"MatrixOptimized", algorithms.FibonacciMatrixOptimized, 100_000},
+		{"MatrixOptimized", algorithms.FibonacciMatrixOptimized, 1_000_000},
+		{"MatrixOptimized", algorithms.FibonacciMatrixOptimized, 10_000_000},
+	}
+
+	for _, unitTest := range unitTests {
+		test.Run(unitTest.name, func(test *testing.T) {
+			_, result := TimeNumber(unitTest.function, unitTest.number)
+			expected, err := readFile(unitTest.number)
+			if err != nil {
+				test.Errorf("%v", err)
+				return
+			}
+			if result[1] != expected {
+				test.Errorf("Expected %s, got %s", expected, result[1])
+			}
+		})
+	}
+}
