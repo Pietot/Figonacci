fe19b6b1ed8e235326c872c4d1d4f4fe9bbac54b
Pietot <102545552+Pietot@users.noreply.github.com>
2025-02-28 17:32:29 +0100

    Added pihedron to main.go

--- main.go
+++ main.go
@@ -52,12 +52,13 @@ func printUsage() {
 	fmt.Println("")
 	fmt.Println("    Available algorithms (from \033[34mfastest\033[0m to \033[31mslowest\033[0m):")
 	fmt.Println("")
-	fmt.Println("        1 - \033[34mfield_extension (fe)\033[0m")
-	fmt.Println("        2 - \033[36mmatrix_optimized (mo)\033[0m")
-	fmt.Println("        3 - \033[32miterative (i)\033[0m")
-	fmt.Println("        4 - \033[33mrecursive_optimized (ro)\033[0m")
-	fmt.Println("        5 - \033[33mmatrix (m)\033[0m")
-	fmt.Println("        6 - \033[31mrecursive (r)\033[0m")
+	fmt.Println("        1 - \033[34mpihedron (p)\033[0m")
+	fmt.Println("        2 - \033[36mfield_extension (fe)\033[0m")
+	fmt.Println("        3 - \033[36mmatrix_optimized (mo)\033[0m")
+	fmt.Println("        4 - \033[32miterative (i)\033[0m")
+	fmt.Println("        5 - \033[33mrecursive_optimized (ro)\033[0m")
+	fmt.Println("        6 - \033[33mmatrix (m)\033[0m")
+	fmt.Println("        7 - \033[31mrecursive (r)\033[0m")
 	fmt.Println("")
 }
 
@@ -137,6 +138,8 @@ func isValidAlgorithm(algorithm string) (func(int, context.Context) *big.Int, st
 		"mo":                  {algorithms.FibonacciMatrixOptimized, "matrix optimized"},
 		"field_extension":     {algorithms.FieldExtension, "field extension"},
 		"fe":                  {algorithms.FieldExtension, "field extension"},
+		"pihedron":            {algorithms.Pihedron, "pihedron"},
+		"p":                   {algorithms.Pihedron, "pihedron"},
 	}
 
 	if entry, exists := validAlgorithms[algorithm]; exists {
