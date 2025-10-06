c7448eac9521606a9c1219488575501378418e63
Pietot <102545552+Pietot@users.noreply.github.com>
2025-10-06 23:33:22 +0200

    Removed sync package (was used when using context)

--- algorithms/recursive_optimized.go
+++ algorithms/recursive_optimized.go
@@ -3,7 +3,6 @@ package algorithms
 
 import (
 	"math/big"
-	"sync"
 )
 
 func FibonacciRecursiveOptimized(n int) *big.Int {
@@ -11,27 +10,21 @@ func FibonacciRecursiveOptimized(n int) *big.Int {
 		0: big.NewInt(0),
 		1: big.NewInt(1),
 	}
-	var mu sync.Mutex
 
 	var recursive func(int) *big.Int
 	recursive = func(n int) *big.Int {
 		if n <= 1 {
 			return big.NewInt(int64(n))
 		}
-		mu.Lock()
 		if val, exist := lookupTable[n]; exist {
-			mu.Unlock()
 			return new(big.Int).Set(val)
 		}
-		mu.Unlock()
 
 		a := recursive(n - 1)
 		b := recursive(n - 2)
 
-		mu.Lock()
 		lookupTable[n] = new(big.Int).Add(a, b)
 		result := new(big.Int).Set(lookupTable[n])
-		mu.Unlock()
 		return result
 	}
 	return recursive(n)
